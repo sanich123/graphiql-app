1e75e893cfbd815b2c8e2b9a7a6e58cab8651c5a
sanich123 <aovoronin.piano@gmail.com>
2023-04-30 20:17:20 +0300

    feat: work in progress

--- src/components/textarea-request/textarea-request.tsx
+++ src/components/textarea-request/textarea-request.tsx
@@ -1,40 +1,88 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import { MutableRefObject } from 'react';
 import styles from './textarea-request.module.scss';
-import { useState, ChangeEvent, KeyboardEvent } from 'react';
+import { useRef, useEffect } from 'react';
 import { useSendGraphiqlResponseMutation } from '@/redux/graphql-api/graphql-api';
 import { changeData } from '@/redux/save-data/save-data';
 import { useAppDispatch } from '@/redux/hooks/hooks';
+import { buildSchema } from 'graphql';
 
 interface TextareaRequestProps {
   up: MutableRefObject<HTMLInputElement | null>;
 }
+
+type CodeMirrorObject = {
+  display: { input: { prevInput: string } };
+};
+
 export function TetxtareaRequest({ up }: TextareaRequestProps) {
-  const { textarea, beautifyDiv } = styles;
-  const [, setRowText] = useState('');
+  const { textarea } = styles;
   const dispatch = useAppDispatch();
   const [sendRequest] = useSendGraphiqlResponseMutation();
+  const codeMirrorRef = useRef(null);
 
-  return (
-    <>
-      <textarea
-        defaultValue={' user(id: 1) { posts { data { id title } } }'}
-        className={textarea}
-        onInput={({ target: { value } }: ChangeEvent<HTMLTextAreaElement>) => setRowText(value)}
-        onKeyDown={async ({ code, ctrlKey, target: { value } }: ChangeEvent<HTMLTextAreaElement> & KeyboardEvent) => {
-          if (code === 'Enter' && ctrlKey) {
-            await sendRequest(value)
+  useEffect(() => {
+    if (codeMirrorRef.current) {
+      require('codemirror/mode/markdown/markdown');
+      require('codemirror/addon/hint/show-hint');
+      require('codemirror/addon/lint/lint');
+      require('codemirror-graphql/hint');
+      require('codemirror-graphql/lint');
+      require('codemirror-graphql/mode');
+      const CodeMirror = require('codemirror');
+      const instance = CodeMirror.fromTextArea(codeMirrorRef.current, {
+        lineNumbers: true,
+        lineWrapping: false,
+        mode: 'graphql',
+        autocorrect: true,
+        extraKeys: {
+          'Ctrl-Enter': async (changeObj: CodeMirrorObject) => {
+            const {
+              display: {
+                input: { prevInput },
+              },
+            } = changeObj;
+            await sendRequest(prevInput)
               .unwrap()
               .then((response) => dispatch(changeData(JSON.stringify(response, null, 1))));
-          }
-        }}
-      />
-      <div className={`${beautifyDiv} topSide`} placeholder="You can type your graphiql requests here" ref={up}>
-        {/* {rowText.split('').map((char, i) => (
-          <div key={`${char}+${i}`} className="char" id={`${i}`}>
-            {char}
-          </div>
-        ))} */}
-      </div>
-    </>
+          },
+        },
+        lint: {
+          schema: buildSchema('type Query { hello: String }'),
+        },
+        hintOptions: {
+          schema: buildSchema('type Query { hello: String }'),
+        },
+        theme: 'material',
+      });
+      // instance.on('inputRead', (changeObj: { display: { input: { prevInput: string } } }) => {
+      //   const {
+      //     display: {
+      //       input: { prevInput },
+      //     },
+      //   } = changeObj;
+      // });
+
+      // instance.on('keyHandled', (codeMirrorObj: CodeMirrorObject, name: string, event: KeyboardEvent) => {
+      //   event.preventDefault();
+      //   const { ctrlKey, code } = event;
+      //   console.log(codeMirrorObj, ctrlKey, code);
+      // });
+    }
+  }, [dispatch, sendRequest]);
+
+  return (
+    <textarea
+      ref={codeMirrorRef}
+      defaultValue={' user(id: 1) { posts { data { id title } } }'}
+      className={textarea}
+      // onKeyDown={async ({ code, ctrlKey, target: { value } }: ChangeEvent<HTMLTextAreaElement> & KeyboardEvent) => {
+      //   if (code === 'Enter' && ctrlKey) {
+      //     await sendRequest(value)
+      //       .unwrap()
+      //       .then((response) => dispatch(changeData(JSON.stringify(response, null, 1))));
+      //   }
+      // }}
+    />
   );
 }
--- src/pages/example.tsx
+++ src/pages/example.tsx
@@ -1,6 +1,11 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import { useEffect, useRef } from 'react';
 import { buildSchema } from 'graphql';
+
+type CodeMirrorObject = {
+  display: { input: { prevInput: string } };
+};
+
 export default function Example() {
   const codeMirrorRef = useRef(null);
   useEffect(() => {
@@ -16,13 +21,40 @@ export default function Example() {
         lineNumbers: true,
         lineWrapping: false,
         mode: 'graphql',
+        autocorrect: true,
+        extraKeys: {
+          'Ctrl-Enter': (changeObj: CodeMirrorObject) => {
+            const {
+              display: {
+                input: { prevInput },
+              },
+            } = changeObj;
+            console.log(prevInput);
+          },
+        },
         lint: {
+          schema: buildSchema('type Query { hello: String }'),
+        },
+        hintOptions: {
           schema: buildSchema('type Query { hello: String }'),
         },
         theme: 'material',
       });
-      instance.setSize('100%', '100%');
+      // instance.on('inputRead', (changeObj: { display: { input: { prevInput: string } } }) => {
+      //   const {
+      //     display: {
+      //       input: { prevInput },
+      //     },
+      //   } = changeObj;
+      // });
+
+      // instance.on('keyHandled', (codeMirrorObj: CodeMirrorObject, name: string, event: KeyboardEvent) => {
+      //   event.preventDefault();
+      //   const { ctrlKey, code } = event;
+      //   console.log(codeMirrorObj, ctrlKey, code);
+      // });
     }
   }, []);
+
   return <textarea ref={codeMirrorRef}></textarea>;
 }
