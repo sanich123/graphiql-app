fb4b57c21aee8e5e42cf966d881a099771ab9100
sanich123 <aovoronin.piano@gmail.com>
2023-05-11 22:10:32 +0300

    feat: add some styles

--- src/components/svg/svg.tsx
+++ src/components/svg/svg.tsx
@@ -239,3 +239,14 @@ export function LogoutIcon() {
     </svg>
   );
 }
+
+export function LoginIcon() {
+  const { theme } = useAppSelector(({ changeThemeLang }) => changeThemeLang);
+  const color = theme === THEMES.light ? COLORS.almostBlack : COLORS.white;
+  return (
+    <svg width="15" height="15" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M20 5H8V9H6V3H22V21H6V15H8V19H20V5Z" fill={color} />
+      <path d="M13.0743 16.9498L11.6601 15.5356L14.1957 13H2V11H14.1956L11.6601 8.46451L13.0743 7.05029L18.024 12L13.0743 16.9498Z" fill={color} />
+    </svg>
+  );
+}
