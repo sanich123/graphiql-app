a2291001a1561d2e511ab1be8a5f5977f6303da3
Maxim Predybailo <predmaxim@ya.ru>
2023-05-25 02:31:14 +0500

    feat: change url from constant

--- src/redux/graphql-api/graphql-api.ts
+++ src/redux/graphql-api/graphql-api.ts
@@ -1,9 +1,10 @@
+import { BASE_URL } from '@/utils/const';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import fetch from 'isomorphic-fetch';
 
 export const graphiqlApi = createApi({
   reducerPath: 'graphiqlApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://graphqlzero.almansi.me', fetchFn: fetch }),
+  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL, fetchFn: fetch }),
   endpoints: (builder) => ({
     sendGraphiqlResponse: builder.mutation({
       query: (graphiqlRequest) => ({
