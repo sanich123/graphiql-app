0d0b8db2ff8b6955ad47f9e43425cd503ec8fd30
Maxim Predybailo <predmaxim@ya.ru>
2023-05-25 02:33:38 +0500

    feat: add textarea for varsEditor

---
+++ src/components/textarea-var/textarea-var.tsx
@@ -0,0 +1,18 @@
+import CodeMirror from '@uiw/react-codemirror';
+import { javascript } from '@codemirror/lang-javascript';
+import { xcodeDark, xcodeLight } from '@uiw/codemirror-theme-xcode';
+import style from './textarea-var.module.scss';
+import { useSelector } from 'react-redux';
+import { RootState } from '@/redux/store';
+
+export function TetxtareaVar() {
+  const defaultValue = 'var';
+  const theme = useSelector((state: RootState) => state.changeThemeLang.theme);
+  const isLightTheme = theme === 'light';
+
+  return (
+    <div className={style.wrapper}>
+      <CodeMirror value={defaultValue} theme={isLightTheme ? xcodeLight : xcodeDark} height="100%" extensions={[javascript({ jsx: true })]} />
+    </div>
+  );
+}
