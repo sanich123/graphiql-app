90559b21fe7afeab1c3c4db33998a6d5c8620e70
Maxim Predybailo <predmaxim@ya.ru>
2023-05-25 02:34:36 +0500

    feat: new layout and styles

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -1,13 +1,39 @@
 import Split from 'react-split';
+import { TetxtareaRequest, TetxtareaVar } from '../../components';
+import { DisplayInfo } from '../../components';
+import { FaPlay, FaAngleRight } from 'react-icons/fa';
+import style from './Editor.module.scss';
+import { BASE_URL } from '@/utils/const';
 
 export function Editor() {
   return (
-    <Split className="flex" style={{ height: 'calc(100vh - 250px)' }}>
-      <Split direction="vertical">
-        <div className="bg-gray-light">1 qwe</div>
-        <div className="bg-gray-light">2 qwe</div>
+    <>
+      <div className={`flex gap-2 items-center border-l-gray-400 ${style.urlWrap}`}>
+        <FaAngleRight />
+        <input type="url" name="urlapi" id="urlapi" defaultValue={BASE_URL} className={`${style.url}`} />
+      </div>
+      <Split className="flex" sizes={[50, 50]} minSize={[300, 300]} gutterSize={8} style={{ height: 'calc(100vh - 250px)' }}>
+        <Split direction="vertical" sizes={[91, 9]} minSize={[200, 60]} gutterSize={8}>
+          <div className={`bg-gray-light ${style.sideEditor}`}>
+            <div className={`${style.queryHeader}`}>
+              <div className={style.title}>Query</div>
+              <button className={style.btnSend}>
+                <FaPlay />
+              </button>
+            </div>
+            <TetxtareaRequest />
+          </div>
+          <div className={`bg-gray-light ${style.sideVar}`}>
+            <div className={`${style.varHeader}`}>
+              <div className={style.title}>Query variables</div>
+            </div>
+            <TetxtareaVar />
+          </div>
+        </Split>
+        <div className={`bg-gray-light ${style.sideDispInfo}`}>
+          <DisplayInfo />
+        </div>
       </Split>
-      <div className="bg-gray-light">2 qwe</div>
-    </Split>
+    </>
   );
 }
