993d4edc8f59ac850f8a5dad6d761ba8fe56180e
sanich123 <aorovonin.piano@gmail.com>
2023-05-24 15:18:01 +0300

    feat: work on private route

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -7,24 +7,33 @@ import { useAppSelector } from '@/redux/hooks/hooks';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '@/firebase';
 import { useRouter } from 'next/router';
+import Loader from '@/components/loader/loader';
 
 export default function Login() {
   const [place, setPlace] = useState(AUTH_PLACES.login);
   const { theme } = useAppSelector(({ changeThemeLang }) => changeThemeLang);
-  const [user] = useAuthState(auth);
+  const [user, loading] = useAuthState(auth);
   const { push } = useRouter();
+
   useEffect(() => {
     if (user) {
-      push(ROUTES.main);
+      push(ROUTES.graphiql);
     }
   }, [user, push]);
+
   return (
     <div className={`authorization--wrapper ${theme}`}>
       <div className="authorization">
         <div className="authorization__wrapper auth-wrapper">
-          <AuthForm place={place} />
-          {place === AUTH_PLACES.login && <SocialBtns />}
-          <AuthNavigateBtns place={place} setPlace={setPlace} />
+          {loading && <Loader />}
+          {!user && !loading && (
+            <>
+              <AuthForm place={place} />
+              {place === AUTH_PLACES.login && <SocialBtns />}
+              <AuthNavigateBtns place={place} setPlace={setPlace} />
+            </>
+          )}
+          {user && <h1>You are logged in so you will be redirect to the editor page</h1>}
         </div>
       </div>
     </div>
