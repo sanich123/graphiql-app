ba2c2e199629266454c2af85c6272ed5a329c7fa
Maxim Predybailo <predmaxim@ya.ru>
2023-05-28 20:27:32 +0500

    refactor: textarea-request component

--- src/components/textarea-request/textarea-request.tsx
+++ src/components/textarea-request/textarea-request.tsx
@@ -1,18 +1,22 @@
 import CodeMirror from '@uiw/react-codemirror';
 import { javascript } from '@codemirror/lang-javascript';
-import { xcodeDark, xcodeLight } from '@uiw/codemirror-theme-xcode';
 import style from './textarea-request.module.scss';
 import { useSelector } from 'react-redux';
 import { RootState } from '@/redux/store';
+import { useCallback, useState } from 'react';
 
 export function TetxtareaRequest() {
-  const defaultValue = 'query {}';
   const theme = useSelector((state: RootState) => state.changeThemeLang.theme);
   const isLightTheme = theme === 'light';
+  const [code, setCode] = useState('query {}');
+
+  const onChangeHandle = useCallback((value: string) => {
+    setCode(value);
+  }, []);
 
   return (
     <div className={style.wrapper}>
-      <CodeMirror value={defaultValue} theme={isLightTheme ? xcodeLight : xcodeDark} height="100%" extensions={[javascript({ jsx: true })]} />
+      <CodeMirror value={code} theme={isLightTheme ? 'light' : 'dark'} height="100%" extensions={[javascript({ jsx: true })]} onChange={onChangeHandle} />
     </div>
   );
 }
