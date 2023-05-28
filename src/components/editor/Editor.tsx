0065051fbbde03bf266a48b9662ee2dd2c800d44
Maxim Predybailo <predmaxim@ya.ru>
2023-05-28 22:09:32 +0500

    feat: add Editor mobile view

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -4,7 +4,7 @@ import { DisplayInfo } from '../../components';
 import { FaPlay, FaAngleRight } from 'react-icons/fa';
 import style from './Editor.module.scss';
 import { BASE_URL } from '@/utils/const';
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 export function Editor() {
   const [editorMinSize, setEditorMinSize] = useState([91, 9]);
@@ -13,14 +13,55 @@ export function Editor() {
     setEditorMinSize(size);
   }, [editorMinSize]);
 
+  const useWidth = () => {
+    const [width, setWidth] = useState(0);
+    const handleResize = () => setWidth(window.innerWidth);
+    useEffect(() => {
+      handleResize();
+      window.addEventListener('resize', handleResize);
+      return () => window.removeEventListener('resize', handleResize);
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+    return width;
+  };
+
+  const windowWidth = useWidth() > 600;
+
   return (
     <>
       <div className={`flex gap-2 items-center ${style.urlWrap}`}>
         <FaAngleRight />
         <input type="url" name="urlapi" id="urlapi" defaultValue={BASE_URL} className={`${style.url}`} />
       </div>
-      <Split className={style.editor} sizes={[50, 50]} minSize={[300, 300]} gutterSize={10} style={{ height: 'calc(100vh - 150px)' }}>
-        <Split direction="vertical" sizes={editorMinSize} minSize={[200, 70]} gutterSize={10}>
+      {windowWidth ? (
+        <Split className={style.editor} sizes={[50, 50]} minSize={[300, 300]} gutterSize={10} style={{ height: 'calc(100vh - 150px)' }}>
+          <Split direction="vertical" sizes={editorMinSize} minSize={[200, 70]} gutterSize={10}>
+            <div className={`${style.sideEditor}`}>
+              <div className={`${style.queryHeader}`}>
+                <div className={style.title} onClick={openEditor}>
+                  Query
+                </div>
+                <button className={style.btnSend}>
+                  <FaPlay />
+                </button>
+              </div>
+              <TetxtareaRequest />
+            </div>
+            <div className={`${style.sideVar}`}>
+              <div className={`${style.varHeader}`}>
+                <div className={style.title} onClick={openEditor}>
+                  Query variables
+                </div>
+              </div>
+              <TetxtareaVar />
+            </div>
+          </Split>
+          <div className={`${style.sideDispInfo}`}>
+            <DisplayInfo />
+          </div>
+        </Split>
+      ) : (
+        <>
           <div className={`${style.sideEditor}`}>
             <div className={`${style.queryHeader}`}>
               <div className={style.title} onClick={openEditor}>
@@ -40,11 +81,11 @@ export function Editor() {
             </div>
             <TetxtareaVar />
           </div>
-        </Split>
-        <div className={`${style.sideDispInfo}`}>
-          <DisplayInfo />
-        </div>
-      </Split>
+          <div className={`${style.sideDispInfo}`}>
+            <DisplayInfo />
+          </div>
+        </>
+      )}
     </>
   );
 }
